3b4d0af40f66ee76379a1e8b54f1305b86f55558
Praneeth Sai Kolla <praneeth.22bce7165@vitapstudent.ac.in>
2025-09-15 08:35:48 +0530

    Profile.tsx

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -41,7 +41,7 @@ const Profile = () => {
                 <div className="flex justify-center mb-4">
                   <Avatar className="w-32 h-32">
                     <AvatarImage 
-                      src={user.profilePicture || ''}  {/* ✅ fixed */}
+                      src={user.profilePicture || ''} 
                       alt={`${user.name} profile picture`}
                       className="object-cover"
                     />
